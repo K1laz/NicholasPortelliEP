99a7c245046e4d67e426beb9fab2136ab6562c0d
unknown <nicholas.portelli.e21506@mcast.edu.mt>
2025-04-05 16:51:17 +0200

    Inital Commit

---
+++ NicholasPortelliEPSolution/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NicholasPortelliEPSolution.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
